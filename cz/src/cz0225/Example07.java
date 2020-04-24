0e659a2be8b4c0238014bb45b22712a104387840
chen-billy <mylovebilly@163.com>
2020-04-24 23:41:21 +0800

    20200226课程作业

---
+++ cz/src/cz0225/Example07.java
@@ -0,0 +1,14 @@
+package cz0225;
+
+/*
+ * if语句的使用
+ */
+public class Example07 {
+	public static void main(String[] args) {
+		int x = 5;
+		if (x < 10) {
+			x++;
+		}
+		System.out.println("x=" + x);
+	}
+}
